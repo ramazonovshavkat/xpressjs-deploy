39a70c37b5dd62477d2019a6af6ad40629e3a438
ramazonovshavkat <shavkatramazonov93@gmail.com>
2024-12-30 11:52:38 +0500

    first commit

---
+++ functions/api.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import Serverless from "serverless-http";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Serverdan Salom");
+});
+
+app.get("/about",(req, res) => {
+    res.json({
+        name:"Shavkat",
+        surname:"Ramazonov",
+        age:34,
+    });
+});
+
+export const handler = Serverless(app)
+ 
